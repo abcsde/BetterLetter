efd6bd1aad6e9cd8d92cda313cda121948a6f10f
abcsde <ruysahu1393@gmail.com>
2024-02-06 18:46:48 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() > 0){
+//         $("header").addClass('sticky');
+//     }else{
+//         $("header").removeClass('sticky');
+//     }
+// });
+
+
+// $(document).ready(function () {
+//     $(".current_page_item").click(function () {
+//       $(".dropdown-menu-value").toggleClass("menu_release");
+//     });
+//   });
